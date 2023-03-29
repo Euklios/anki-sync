b44da4a515c9cd4f1e38603ee0935c999c591ddc
Joel Widmer <joel@widmer.me>
2023-03-29 17:28:58 +0900

    Load anki.host and anki.pronunciation_field from settings

--- anki.py
+++ anki.py
@@ -1,5 +1,4 @@
 import json
-from typing import Dict
 
 import requests
 
@@ -7,13 +6,10 @@ from data.NoteDetails import NoteDetails
 from data.Settings import Settings
 from utils import map_fields
 
-ANKI_HOST = "http://localhost:8765/"
-PRONUNCIATION_FIELD = 'Pronunciation'
-
 
 def anki_request(settings: Settings, action: str, **params):
     request_payload = json.dumps({'action': action, 'params': params, 'version': 6}).encode('UTF-8')
-    response = requests.request('get', ANKI_HOST, data=request_payload)
+    response = requests.request('get', settings.anki.host, data=request_payload)
 
     if 200 > response.status_code or response.status_code >= 300:
         raise IOError("Failed to communicate with anki")
@@ -35,9 +31,9 @@ def update_anki_note(settings: Settings, note, query: str, content_info: NoteDet
 
     if content_info.pronunciation:
         anki_request(settings, 'storeMediaFile', filename=filename, url=content_info.pronunciation)
-        fields[PRONUNCIATION_FIELD] = f'[sound:{filename}]'
+        fields[settings.anki.settings.anki.pronunciation_field] = f'[sound:{filename}]'
     else:
-        fields[PRONUNCIATION_FIELD] = ''
+        fields[settings.anki.settings.anki.pronunciation_field] = ''
 
     anki_request(settings, 'updateNoteFields', note={
         'id': note['noteId'],
--- data/Settings.py
+++ data/Settings.py
@@ -9,6 +9,7 @@ class AnkiSettings:
     host: str = "http://localhost:8765/"
     target_deck: str = None
     question_field: str = "Question"
+    pronunciation_field: str = 'Pronunciation'
     field_mappings: Dict[str, str] = {
         'meaning_mnemonic': 'Meaning Mnemonic',
         'reading_mnemonic': 'Reading Mnemonic'
