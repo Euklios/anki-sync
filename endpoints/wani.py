79032343773cf47bc7e6d7a456c2753d57261ef8
Joel Widmer <joel@widmer.me>
2023-03-30 01:53:55 +0900

    Make Wanikani a valid NoteSource

--- endpoints/wani.py
+++ endpoints/wani.py
@@ -1,16 +1,26 @@
 import typing
+from typing import Iterable
 
 import requests
 
-from data.NoteDetails import NoteDetails
+from data.NoteDetails import NoteDetails, NoteSource
 from endpoints.abc.BaseProvider import BaseProvider
+from endpoints.abc.NoteProvider import BaseNoteProvider
 
 
 class WanikaniSettings:
     api_token: str = None
 
 
-class Wanikani(BaseProvider):
+class Wanikani(BaseProvider, BaseNoteProvider):
+    def list_notes(self) -> Iterable[NoteDetails]:
+        assignments = list(self._load_assignments())
+
+        for assignment in assignments:
+            subject = list(filter(lambda e: e['id'] == assignment['data']['subject_id'], self.subjects))[0]
+
+            yield self._convert_subject_to_note_details(subject)
+
     @staticmethod
     def config_class() -> typing.Type:
         return WanikaniSettings
@@ -32,6 +42,19 @@ class Wanikani(BaseProvider):
             raise IOError(f"Failed to load resource {path}. [{response.status_code}]: {response.text}")
 
         return response.json()
+
+    def _load_assignments(self):
+        next_path = "assignments?subject_types=vocabulary&started=true"
+
+        while True:
+            response = self._get(next_path)
+            yield from response['data']
+
+            if response['pages']['next_url'] is None:
+                return
+
+            # TODO: This will most likely fail. The base path needs to be removed first
+            next_path = response['pages']['next_url']
 
     def _load_subjects(self):
         total_count = 1
@@ -59,3 +82,16 @@ class Wanikani(BaseProvider):
         details.fields['reading_mnemonic'] = vocab['data']['reading_mnemonic']
         details.tags.add(f"wanikani{vocab['data']['level']}")
         details.pronunciation = vocab['data']['pronunciation_audios'][0]['url']
+
+    def _convert_subject_to_note_details(self, subject) -> NoteDetails:
+        details = NoteDetails()
+        details.source = NoteSource(self.endpoint_name(), subject['id'])
+
+        details.fields['Question'] = subject['data']['slug']
+        details.fields['Answer'] = ", ".join([meaning['meaning'] for meaning in subject['data']['meanings'] if meaning['accepted_answer']])
+        details.fields['meaning_mnemonic'] = subject['data']['meaning_mnemonic']
+        details.fields['reading_mnemonic'] = subject['data']['reading_mnemonic']
+        details.tags.add(f"wanikani{subject['data']['level']}")
+        details.pronunciation = subject['data']['pronunciation_audios'][0]['url']
+
+        return details
